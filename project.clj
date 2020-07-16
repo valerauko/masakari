a949171484d1079a6372436c2f51a194a0268751
Vale <valerauko@gmail.com>
2020-07-16 17:45:43 +0900

    Set up to compile for graal-native

--- project.clj
+++ project.clj
@@ -1,10 +1,19 @@
-(defproject masakari "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject net.valerauko/masakari "0.8.0"
+  :description "Tool to detect unused entries in your dictionary files."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [metosin/jsonista "0.2.6"]]
+  :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
   :main ^:skip-aot masakari.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+  :native-image
+  {:name "masakari"
+   :opts ["--no-server"
+          "--no-fallback"
+          "--report-unsupported-elements-at-runtime"
+          "--initialize-at-build-time"
+          "--allow-incomplete-classpath"]})
