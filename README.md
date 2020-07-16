a7fcb0f81e167d3f456190428abfe02417c2b608
Vale <valerauko@gmail.com>
2020-07-16 17:45:53 +0900

    Update metadata

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# masakari
+# Masakari
+
+Tool to detect unused entries in your dictionary files.
+
+## Usage
+
+Download `masakari` executable from releases. Execute.
+
+```
+masakari /path/to/folder/to/check
+```
+
+If you omit the path argument it will use the current folder (`.`).
+
+## Limitations
+
+Currently only works with Vue.js code and JSON dictionary files. It assumes that every JSON file is a dictionary.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.valerauko/masakari "0.8.0"
   :description "Tool to detect unused entries in your dictionary files."
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "AGPL-3.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.gnu.org/licenses/agpl.txt"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [metosin/jsonista "0.2.6"]]
   :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
